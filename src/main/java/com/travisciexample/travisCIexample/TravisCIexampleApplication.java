deeef1a8b2bb6ddae0049dda382c9caaefcd6836
Yasrab Memon <yasrab.memon@royalcyber.com>
2022-07-05 17:19:32 +0500

    first commit to practice travis

---
+++ src/main/java/com/travisciexample/travisCIexample/TravisCIexampleApplication.java
@@ -0,0 +1,13 @@
+package com.travisciexample.travisCIexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravisCIexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravisCIexampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/travisciexample/travisCIexample/TravisCIexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travisciexample.travisCIexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravisCIexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
